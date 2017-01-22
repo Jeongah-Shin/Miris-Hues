a1dafff2d5ee1cab98f8cd132ce770d427bc80a8
KimBoWoon <haioer@naver.com>
2017-01-22 16:10:20 +0900

    init complate

---
+++ app/Http/Controllers/Cognitive/CognitiveAPI.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: secret
+ * Date: 1/22/17
+ * Time: 4:05 PM
+ */
+
+namespace App\Http\Controllers\Cognitive;
+
+use App\Http\Controllers\Controller;
+
+require_once 'HTTP/Request2.php';
+
+class CognitiveAPI extends Controller
+{
+    public function index()
+    {
+        return view('cognitive/cognitive');
+    }
+
+    public function showText()
+    {
+        $request = new \Http_Request2('https://api.projectoxford.ai/vision/v1.0/ocr');
+        $url = $request->getUrl();
+        $headers = array(
+            // Request headers
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => env('COGNITIVE_VISION_KEY'),
+        );
+        $request->setHeader($headers);
+        $parameters = array(
+            // Request parameters
+            'language' => 'en',
+            'detectOrientation ' => 'true',
+        );
+        $url->setQueryVariables($parameters);
+        $request->setMethod(\HTTP_Request2::METHOD_POST);
+        // Request body
+        $urlData = array('url' => "https://c2.staticflickr.com/6/5283/5685590377_23207b673d_b.jpg");
+        $urlDataJsonEncode = json_encode($urlData);
+        $request->setBody($urlDataJsonEncode);
+        try {
+            $response = $request->send();
+            echo $response->getBody();
+        } catch (HttpException $ex) {
+            echo $ex;
+        }
+    }
+
+    public function showTag()
+    {
+        $request = new \Http_Request2('https://api.projectoxford.ai/vision/v1.0/tag');
+        $url = $request->getUrl();
+        $headers = array(
+            // Request headers
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => env('COGNITIVE_VISION_KEY'),
+        );
+        $request->setHeader($headers);
+        $parameters = array(// Request parameters
+        );
+        $url->setQueryVariables($parameters);
+        $request->setMethod(\HTTP_Request2::METHOD_POST);
+        // Request body
+        $urlData = array('url' => "https://c2.staticflickr.com/6/5283/5685590377_23207b673d_b.jpg");
+        $urlDataJsonEncode = json_encode($urlData);
+        $request->setBody($urlDataJsonEncode);
+        try {
+            $response = $request->send();
+            echo $response->getBody();
+        } catch (HttpException $ex) {
+            echo $ex;
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/info', 'Cognitive\CognitiveAPI@index');
+Route::get('/text', 'Cognitive\CognitiveAPI@showText');
+Route::get('/tag', 'Cognitive\CognitiveAPI@showTag');
+
+Route::get('/uploadfile', 'File\UploadFileController@index');
+Route::post('/uploadfile', 'File\UploadFileController@showUploadFile');
+
+Route::get('images/{filename}', function ($filename) {
+    $path = storage_path() . '\public\\' . $filename;
+
+    echo $path;
+    echo '<br>';
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
